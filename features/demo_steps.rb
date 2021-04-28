ab029cc71844cbd851f94ff800e9c78c7f83bf79
Saul Soto <saulivan852@gmail.com>
2021-04-28 12:40:18 -0700

    Created variables of frecuetly used values

--- features/demo_steps.rb
+++ features/demo_steps.rb
@@ -3,19 +3,21 @@ require 'rest-client'
 require 'json'
 require "uri"
 require "net/http"
-url = URI("http://3.212.6.186:3000/api/get_id_information")
+$url_base = "http://3.212.6.186:3000/";
 $token;
 $id_info;
 $body;
+$user_full = 'creditario-full1@gmail.com';
+$password_full = 'creditario123';
 
 Given("The user gets token") do
-$token =  RestClient.post "http://3.212.6.186:3000/api/login", {'auth' => {'email' => "creditario@arkusnexus.com",'password' => "creditario123" }}.to_json, {content_type: :json, accept: :json}
+$token =  RestClient.post $url_base + 'api/login', {'email' => $user_full,'password' => $password_full }.to_json, {content_type: :json, accept: :json}
 puts "Login successful!"
 $token = $token[8..136]
 end
 
 Given("The user sends both sides") do
-$id_info = RestClient.post "http://3.212.6.186:3000/api/get_id_information", {:front_id => File.new("C:\\Users\\Arkus\\Pictures\\Dataset-Creditario\\Dataset\\INE\\295.png", 'rb'),:back_id => File.new('C:\\Users\\Arkus\\Pictures\\Dataset-Creditario\\Dataset\\INE\\295_R.png', 'rb')}, {Authorization: $token,content_type: 'multipart/form-data', accept: :json}
+$id_info = RestClient.post $url_base + 'api/get_id_information', {:front_id => File.new("C:\\Users\\Arkus\\Pictures\\Dataset-Creditario\\Dataset\\INE\\295.png", 'rb'),:back_id => File.new('C:\\Users\\Arkus\\Pictures\\Dataset-Creditario\\Dataset\\INE\\295_R.png', 'rb')}, {Authorization: $token,content_type: 'multipart/form-data', accept: :json}
 $id_info.to_json
 $body  = $id_info.body
 $body = JSON.parse($body)
