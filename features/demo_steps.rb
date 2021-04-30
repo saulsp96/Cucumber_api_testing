2dc40f55904c7d0d2ed250d1e2a1cd9d0e76fafe
Saul Soto <saulivan852@gmail.com>
2021-04-30 09:49:20 -0700

    Upddated steps

--- features/demo_steps.rb
+++ features/demo_steps.rb
@@ -14,17 +14,27 @@ $password_full = 'creditario123';
 
 Given("The user gets token") do
 $token =  RestClient.post $url_base + 'api/login', {'email' => $user_full,'password' => $password_full }.to_json, {content_type: :json, accept: :json}
-$token = $token[8..136]
+$token = JSON.parse($token)
+$token = $token['jwt']
 end
 
 Given("The user sends both sides") do
 $id_info = RestClient.post $url_base + 'api/get_id_information', {:front_id => File.new("C:\\Users\\Arkus\\Pictures\\Dataset-Creditario\\Dataset\\INE\\295.png", 'rb'),:back_id => File.new('C:\\Users\\Arkus\\Pictures\\Dataset-Creditario\\Dataset\\INE\\295_R.png', 'rb')}, {Authorization: $token,content_type: 'multipart/form-data', accept: :json}
-$id_info.to_json
-$body  = $id_info.body
-$body = JSON.parse($body)
+puts "Service response is OK"
+end
+
+Then("The service returns expected responde") do
+  $id_info.to_json
+  $body  = $id_info.body
+  $body = JSON.parse($body)
+end
+
+And("Response matches data") do
+  puts "This are body keys"
 puts $body.keys
 $front = $body['front']
 # $front = JSON.parse($front)
+puts "This are front keys"
 puts $front.keys
 # puts $front.keys
 # $back = $body.back
@@ -33,13 +43,6 @@ puts $front.keys
 ##$front = $body['front']
 ##$front = JSON.parse(front)
 ##puts front
-end
-
-Then("The service returns expected responde") do
-
-end
-
-And("Response matches data") do
 end
 
 def say(options = {})
